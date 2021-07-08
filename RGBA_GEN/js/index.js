264d75485f3bbce55f5d7e87155a4febb7ec7210
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-07-08 15:03:59 -0500

    Update index.js
    Made the copy function and the change color rgba  function, they dont work at the moment but im workin on it

--- RGBA_GEN/js/index.js
+++ RGBA_GEN/js/index.js
@@ -1,14 +1,21 @@
-//getValue()
-//copy()
-let outputBox = document.querySelector(".output-text");
-let bd = document.body;
+let outputBox = document.querySelector(".output");
 
 function getColorValue() {
-  let red = document.getElementById("red-inp").value;
-  let green = document.getElementById("green-inp").value;
-  let blue = document.getElementById("blue-inp").value;
-  let alpha = document.getElementById("alpha-inp").value;
-  bd.style.background = `rgba(${red},${green},${blue},${alpha})`;
+  let red = document.getElementById("red").value;
+  let green = document.getElementById("green").value;
+  let blue = document.getElementById("blue").value;
+  let alpha = document.getElementById("alpha").value;
+  documen.body.style.background =
+    "rgba(" + red + "," + blue + "," + green + "," + alpha + ")";
   outputBox.innerHTML = `rgba(${red},${green},${blue},${alpha})`;
 }
-function copy(params) {}
+
+function copyColor() {
+  let textColor = document.createElement("text-area");
+  textColor.value = outputBox.textContent;
+  document.body.appendChild(textColor);
+  textColor.select();
+  document.exeCommand("copy");
+  document.body.removeChild(textColor);
+  alert("COLOR COPIED!!");
+}
